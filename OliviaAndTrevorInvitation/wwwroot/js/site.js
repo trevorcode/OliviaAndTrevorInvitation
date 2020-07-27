cc508b3c5e126dc6589ff936c863f9ce71d5c3ef
Trevor Spencer <trevor@admonex.com>
2020-07-27 17:05:45 -0600

    Updated Schedule and fixed count down

--- OliviaAndTrevorInvitation/wwwroot/js/site.js
+++ OliviaAndTrevorInvitation/wwwroot/js/site.js
@@ -1,5 +1,5 @@
 ﻿// Set the date we're counting down to
-var countDownDate = new Date("Aug 1, 2020 17:00:00").getTime();
+var countDownDate = new Date("Aug 1, 2020 18:00:00").getTime();
 
     // Update the count down every 1 second
 var x = setInterval(function() {
